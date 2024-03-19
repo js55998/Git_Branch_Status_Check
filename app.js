af98d4ed4b8299dd047866d6d26f5ce5f53dd260
debasish <debasish.sahoo96@gmail.com>
2024-03-19 14:00:56 +0530

    Add Express

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ taskManager.completeTask(1)
 taskManager.listTasks().forEach((task, index) => {
     console.log(`Task ${index + 1}: ${task.task} (Completed: ${task.completed})`);
 });
+
+const app = require('express')();
+
+app.use(/\/admin\/.*/, (req, res, next) => {
+    if (!req.user.isAdmin) {
+        res.status(401).send('Unauthorized');
+    } else {
+        next();
+    }
+});
+
+app.get('/admin/users/:id', (req, res) => {
+    res.send(app.database.users[req.params.id]);
+});
