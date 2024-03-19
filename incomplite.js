d6aa7be65b114a86316d8b967ad74d6804628743
debasish <debasish.sahoo96@gmail.com>
2024-03-19 14:22:10 +0530

    Add incomplete

--- incomplite.js
+++ incomplite.js
@@ -1,2 +1,10 @@
 function findBox() {
     return $("box.important
+
+
+    async function loadMessage() {
+            const query = new URLSearchParams(location.search);
+            const url = '/api/messages/' + query.get('message_id');
+            const data = await (await fetch(url)).json();
+            document.getElementById('message').innerHTML = data.html;
+        }
